ad0352d83cc6be7a94fde9cdb129251d89a5b5f8
Eduardo-brito1 <kabisashi2@gmail.com>
2022-12-10 14:47:12 -0300

    utilizand o put

--- src/controladores/instrutores.js
+++ src/controladores/instrutores.js
@@ -42,8 +42,38 @@ const cadastrarinstrutores = ((req, res) => {
     return res.status(201).json(instrutor);
 })
 
+const atualizarInstrutores = ((req, res) => {
+    const { id } = req.params;
+    const { nome, email, status } = req.body;
+
+    if (!nome) {
+        return res.status(400).json({ mensagem: 'nome e obrigatorio' })
+    }
+
+    if (!email) {
+        return res.status(400).json({ mensagem: 'email e obrigatorio' })
+    }
+
+
+    const instrutor = instrutores.find((instrutor) => {
+        return instrutor.id === Number(id);
+    });
+
+    if (!instrutor) {
+        return res.status(404).json({ mensagem: 'instrutor não encontrado' });
+    }
+
+    instrutor.nome = nome;
+    instrutor.email = email;
+    instrutor.status = status;
+
+    return res.status(203).send()
+
+})
+
 module.exports = {
     listarInstrutores,
     obterinstrutor,
-    cadastrarinstrutores
+    cadastrarinstrutores,
+    atualizarInstrutores
 }
--- src/rotas.js
+++ src/rotas.js
@@ -3,8 +3,10 @@ const instrutores = require('./controladores/instrutores');
 
 const rotas = express();
 
-rotas.get('/instrutores', instrutores.listarInstrutores)
-rotas.get('/instrutores/:id', instrutores.obterinstrutor)
-rotas.post('/instrutores', instrutores.cadastrarinstrutores)
+rotas.get('/instrutores', instrutores.listarInstrutores)//ver a lista de instrutores
+rotas.get('/instrutores/:id', instrutores.obterinstrutor)//procurar instrutor pelo id
+rotas.post('/instrutores', instrutores.cadastrarinstrutores)//cadastrar instrutores
+rotas.put('/instrutores/:id', instrutores.atualizarInstrutores)//atualiza todo as informações de um recurso
+rotas.patch('/instrutores/:id',)
 
 module.exports = rotas;
